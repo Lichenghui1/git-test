792a07b5a6058fd0f585d64e26d75f54ba688a04
hui <3010895008@qq.com>
2022-03-24 22:52:49 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author hui
+ * @create 2022-03-24 22:49
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello,git");
+    }
+}
