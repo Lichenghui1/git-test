2ed2ba2297495d29824d025c3ad48720562f18c8
hui <3010895008@qq.com>
2022-11-29 01:04:24 +0800

    first commit

---
+++ src/main/java/com/itcast/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.itcast.git;
+
+/**
+ * @author hui
+ * @create 2022-11-29 1:00
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("111111");
+    }
+}
